61aa97de8fe639424de2aa1e85cefb289280e85b
ghgh10288 <ghgh10288@naver.com>
2025-05-07 14:36:30 +0900

    테스트

---
+++ Assets/Script/Map/SectionData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SectionData : MonoBehaviour {
+    public int id;
+    public char rate;
+    public string eventType;
+    public bool isVisited;
+
+
+    public int stepCost;
+}
