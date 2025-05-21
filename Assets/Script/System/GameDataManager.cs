d6cba02c67095381e849642cf29179b38092fd67
ghgh10288 <ghgh10288@naver.com>
2025-05-21 18:58:48 +0900

    폴더의 파일을 읽고 그 데이터를 기반으로 랜덤 점 생성

--- Assets/Script/System/GameDataManager.cs
+++ Assets/Script/System/GameDataManager.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameDataManager : MonoBehaviour {
+    public static GameDataManager Instance { get; private set; }
     public GameData gameData;
 
+    public static GameData Data => Instance.gameData;
+
+    void Awake() {
+        if (Instance != null && Instance != this)  {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     void Start() {
-        if(gameData == null) {
+        if (gameData == null) {
             Debug.LogError("GameData is None");
             return;
         }
 
         gameData.playerName = "Potato";
-        gameData.seed = 12345;
+        gameData.seed = 54321;
         gameData.areaCount = 1;
-    }   
+        gameData.initialMinDistance = 3f;
+
+        gameData.areaDataFolderPath = "Assets/Resources/AreaData";
+    }
 }
