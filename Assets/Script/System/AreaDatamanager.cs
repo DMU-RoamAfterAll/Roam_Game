240d4c320d3792772c67b8b86c7658adcb0eb683
ghgh10288 <ghgh10288@naver.com>
2025-05-26 13:08:39 +0900

    랜덤 점 찍기 구체화

--- Assets/Script/System/AreaDatamanager.cs
+++ Assets/Script/System/AreaDatamanager.cs
@@ -10,18 +10,5 @@ public class AreaDataManager : MonoBehaviour {
         }
 
         RandomSectionSpawner randomSectionSpawner = this.gameObject.AddComponent<RandomSectionSpawner>();
-        
-        CreateEdge();
-    }
-
-    void CreateEdge() {
-        foreach(Vector2 point in  areaData.edgePoint) {
-            GameObject go = Instantiate(GameDataManager.Data.edgePrefab, point, Quaternion.identity);
-            go.transform.SetParent(this.gameObject.transform);
-        }
-    }
-
-    void CreateRandomSection() {
-        RandomSectionSpawner randomSectionSpawner = this.gameObject.AddComponent<RandomSectionSpawner>();
     }
 }
