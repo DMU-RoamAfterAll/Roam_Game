c7fdc663d2848602ea56aad9eb0220b321be4e09
ghgh10288 <ghgh10288@naver.com>
2025-05-28 15:24:53 +0900

    랜덤 점 찍기 미완성

--- Assets/Script/Map/Section/SectionData.cs
+++ Assets/Script/Map/Section/SectionData.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class SectionData : MonoBehaviour {
-    public int id;
+    public string id;
     public char rate;
     public string eventType;
     public bool isVisited;
