4c16e8402f541e7cb7e7e019d3c186a09ce44238
ghgh10288 <ghgh10288@naver.com>
2025-06-09 15:58:53 +0900

    걸음 수 코드 수정

--- Assets/Script/Map/Area/AreaLocateControl.cs
+++ Assets/Script/Map/Area/AreaLocateControl.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Linq;
 using System.IO;
+using System.Collections;
 
 public class AreaLocateControl : MonoBehaviour {
     [Header("Data")]
@@ -95,14 +96,46 @@ public class AreaLocateControl : MonoBehaviour {
             basePoint[i] = new Vector2(x, y);
         }
 
-        MoveArea();
+        StartCoroutine(MoveArea());
     }
 
-    void MoveArea() {
-        for(int i = 0; i < totalAreaCount; i++) {
-            spawners[i].gameObject.transform.position = basePoint[i];
+    IEnumerator MoveArea() {
+        int count = totalAreaCount;
+        Vector2[] starts = new Vector2[count];
+        Vector2[] ends = new Vector2[count];
+        float[] durations = new float[count];
+        float[] elapsed = new float[count];
+
+        // 초기 위치와 목표 위치, 시간 설정
+        for (int i = 0; i < count; i++) {
+            starts[i] = spawners[i].transform.position;
+            ends[i] = basePoint[i];
+            durations[i] = Random.Range(0.5f, 1.2f);
+            elapsed[i] = 0f;
         }
 
-        LinkSectionSpawner linkSectionSpawner = this.gameObject.AddComponent<LinkSectionSpawner>();
+        bool allDone = false;
+
+        while (!allDone) {
+            allDone = true;
+
+            for (int i = 0; i < count; i++) {
+                if (elapsed[i] < durations[i]) {
+                    elapsed[i] += Time.deltaTime;
+                    float t = Mathf.Clamp01(elapsed[i] / durations[i]);
+                    spawners[i].transform.position = Vector2.Lerp(starts[i], ends[i], t);
+                    allDone = false; // 아직 이동 중인 스포너가 있음
+                }
+            }
+
+            yield return null; // 다음 프레임까지 대기
+        }
+
+        // 끝 위치 고정
+        for (int i = 0; i < count; i++) {
+            spawners[i].transform.position = ends[i];
+        }
+
+        this.gameObject.AddComponent<LinkSectionSpawner>();
     }
 }
