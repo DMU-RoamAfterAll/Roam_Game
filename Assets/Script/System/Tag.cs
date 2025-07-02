e236e83174901443015ef9bfd65a699d1e2cf8b0
ghgh10288 <ghgh10288@naver.com>
2025-07-02 16:46:18 +0900

    Revert "시야작업 기초"
    This reverts commit a89433bfb6d87fbabe0ced3d73b13c2fe7b32d8f.

--- Assets/Script/System/Tag.cs
+++ Assets/Script/System/Tag.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public enum TagName {
     Area,
-    Tutorial
+    Tutorial,
+    Section,
+    LinkSection,
+    MainSection
 }
 
 public static class Tag {
     public static string Area => TagName.Area.ToString();
     public static string Tutorial => TagName.Tutorial.ToString();
+    public static string Section => TagName.Section.ToString();
+    public static string LinkSection => TagName.LinkSection.ToString();
+    public static string MainSection => TagName.MainSection.ToString();
 }
