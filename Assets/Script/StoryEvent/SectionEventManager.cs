50a2674d033d5d9723bc9f2bb66e667ff3ff2788
Seo-YunJeong <130148628+SeoYunJoung@users.noreply.github.com>
2025-09-22 20:49:35 +0900

    전투 시스템 2차 : 메인 key노드에 Battle노드 추가, 파싱 실행 완료

--- Assets/Script/StoryEvent/SectionEventManager.cs
+++ Assets/Script/StoryEvent/SectionEventManager.cs
@@ -32,7 +32,6 @@ public class ActionNode
     public List<WeaponData> lostW; //무기 유실 기능을 위한 노드, <"무기 코드", 갯수> 형식으로 작성
     public List<FlagData> flagSet; //플래그 설정을 위한 플래그 명을 작성을 위한 노드, <"플래그명", boolean>형식으로 작성
     public List<FlagData> flagCheck; //본문을 내보내기 위해 플래그를 확인을 위한 노드, <"플래그명", boolean>형식으로 작성
-                                      //(리스트 형식으로 복수 체크 가능)
 }
 
 [System.Serializable]
@@ -81,6 +80,19 @@ public class MenuOption : CommonNode
 {
     public string id; //선택지를 구분하기 위한 키값
     public string label; //선택지 내용
+}
+
+//-------------------------------------------------------------------------------
+
+[System.Serializable]
+//전투씬을 출력하는 노드, Battle1 Battle2 ... 등으로 작성
+public class BattleNode
+{
+    public List<string> battleIntro; //전투 시작 전 인트로 내용을 적는 노드, 리스트를 사용하여 여러 문장을 나누어 작성
+    public List<string> battleOrder; //전투 진행 순서를 결정하는 노드, 리스트 앞쪽 순서부터 순서대로 진행
+    public List<string> battleTriggers; //전투시 적용할 특성
+    public string BattleWin; //전투 승리 시 다음 출력을 위한 노드 키값, 본문이 출력된 후 해당 노드로 이동
+    public string BattleLose; //전투 패배 시 다음 출력을 위한 노드 키값, 본문이 출력된 후 해당 노드로 이동
 }
 
 //-------------------------------------------------------------------------------
@@ -179,7 +191,12 @@ public class SectionEventManager : MonoBehaviour
                 MenuNode menu = nodeObj.ToObject<MenuNode>();
                 sectionData[key] = menu; //키가 존재하지 않으면 동적 추가
             }
-            else if (key.StartsWith("Text") || key.StartsWith("Result"))
+            else if (key.StartsWith("Battle"))
+            {
+                BattleNode battle = nodeObj.ToObject<BattleNode>();
+                sectionData[key] = battle; //키가 존재하지 않으면 동적 추가
+            }
+            else if (key.StartsWith("Text") || key.StartsWith("Result") || key.StartsWith("PostBattle"))
             {
                 // action 빼고 복제본 만들기
                 JObject nodeClone = (JObject)nodeObj.DeepClone();
@@ -228,6 +245,18 @@ public class SectionEventManager : MonoBehaviour
     }
 
     /// <summary>
+    /// 전투씬 노드를 꺼내는 메소드
+    /// </summary>
+    /// <param name="key">꺼낼 전투씬 노드의 key값</param>
+    /// <returns>해당 전투씬 노드</returns>
+    public BattleNode GetBattleNode(string key)
+    {
+        if (sectionData.TryGetValue(key, out object node) && node is BattleNode battle)
+            return battle;
+        return null;
+    }
+
+    /// <summary>
     /// 스크립트 타이핑 효과 코루틴
     /// </summary>
     /// <param name="fullText">타이핑 효과를 넣고 싶은 텍스트 전문</param>
@@ -256,7 +285,7 @@ public class SectionEventManager : MonoBehaviour
     /// 액션 노드 처리 메소드
     /// </summary>
     /// <param name="actions">대상 액션 노드</param>
-    void HandleNodeActions(ActionNode actions)
+    private void HandleNodeActions(ActionNode actions)
     {
         if (actions == null)
         {
@@ -344,11 +373,11 @@ public class SectionEventManager : MonoBehaviour
 
                     //테스트 출력
                     Debug.Log($"\'{itemData.name}\'아이템을 {amount}개 잃었습니다.");
-                    StartCoroutine(userDataManager.LostItem(itemData.code,amount)); //api 메소드
+                    StartCoroutine(userDataManager.LostItem(itemData.code, amount)); //api 메소드
                 }
             }
         }
-        
+
         //무기 체크
         if (actions.checkW != null && actions.checkW.Count > 0 && actions.checkW is List<WeaponData> checkWData)
         {
@@ -415,7 +444,7 @@ public class SectionEventManager : MonoBehaviour
 
                     //테스트 출력
                     Debug.Log($"\'{weaponData.name}\'무기를 {amount}개 잃었습니다.");
-                    StartCoroutine(userDataManager.LostWeapon(weaponData.code,amount)); //api 메소드
+                    StartCoroutine(userDataManager.LostWeapon(weaponData.code, amount)); //api 메소드
                 }
             }
         }
@@ -432,7 +461,7 @@ public class SectionEventManager : MonoBehaviour
 
                     //테스트 출력
                     Debug.Log($"\'{FlagData.name}\'플래그 상태를 {flagState}로 변경했습니다.");
-                    StartCoroutine(userDataManager.FlagSet(flagCode,flagState)); //api 메소드
+                    StartCoroutine(userDataManager.FlagSet(flagCode, flagState)); //api 메소드
                 }
             }
         }
@@ -448,10 +477,12 @@ public class SectionEventManager : MonoBehaviour
                     storyFlagNode FlagData = storyFlagManager.GetFlagByCode(flagCode);
 
                     StartCoroutine(userDataManager.FlagCheck( //api 메소드
-                        onResult: list => {
+                        onResult: list =>
+                        {
                             foreach (var it in list)
                             {
-                                if (flagCode == it.flagCode) {
+                                if (flagCode == it.flagCode)
+                                {
                                     if (flagState == it.flagState)
                                     {
                                         Debug.Log($"{FlagData.name}는 {flagState}를 만족합니다."); //테스트 출력
@@ -465,7 +496,7 @@ public class SectionEventManager : MonoBehaviour
                                 }
                             }
                         },
-                        onError: (code,msg) => Debug.LogError($"[{GetType().Name}] 플래그 불러오기 실패: {code}/{msg}")
+                        onError: (code, msg) => Debug.LogError($"[{GetType().Name}] 플래그 불러오기 실패: {code}/{msg}")
                         )
                     );
                 }
@@ -502,6 +533,10 @@ public class SectionEventManager : MonoBehaviour
             else if (node is MenuNode menuNode)
             {
                 DisplayMenuNode(menuNode);
+            }
+            else if (node is BattleNode battleNode)
+            {
+                DisplayBattleNode(battleNode);
             }
             else
             {
@@ -542,6 +577,7 @@ public class SectionEventManager : MonoBehaviour
             StartCoroutine(TypeTextCoroutine(string.Join("\n", node.value)));
         }
     }
+
     void DisplayMenuNode(MenuNode node)
     {
         //텍스트 비우기
@@ -552,6 +588,7 @@ public class SectionEventManager : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
+
         foreach (MenuOption option in node.menuOption)
         {
             //각 선택지에 대해 버튼 생성
@@ -591,6 +628,19 @@ public class SectionEventManager : MonoBehaviour
                     Debug.Log($"[{GetType().Name}] MenuNode의 next 값이 없습니다. 종료 또는 대기 처리 필요.");
                 }
             });
+        }
+    }
+
+    void DisplayBattleNode(BattleNode node)
+    {
+        Debug.Log("배틀 실행");
+        //텍스트 비우기
+        dialogueText.text = "일단 실행 확인";
+
+        //기존 버튼 제거
+        foreach (Transform child in buttonPanel)
+        {
+            Destroy(child.gameObject);
         }
     }
     //-------------------------------------------------------------------------------
